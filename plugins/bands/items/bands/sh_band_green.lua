360762b2eb48e6abaeba4397009e25dfce301425
Dunkersplay <56454629+Dunkersplay@users.noreply.github.com>
2020-04-15 16:31:42 -0600

    Update sh_band_green.lua

--- plugins/bands/items/bands/sh_band_green.lua
+++ plugins/bands/items/bands/sh_band_green.lua
@@ -1,5 +1,5 @@
-ITEM.name = "Zielona Opaska"
+ITEM.name = "Green Loyalist Armband"
 ITEM.model = "models/props_lab/clipboard.mdl"
-ITEM.description = "Zielona opaska lojalisty."
+ITEM.description = "A green armband given to Green Tier Loyalists."
 ITEM.category = "Bands"
 ITEM.band = "green"
